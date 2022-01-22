cec12efdc53e001949a166ec3f52af7a230f89b1
“Libby <2565119c@student.gla.ac.uk>
2022-01-22 19:29:01 +0000

    Chapter 4: Commit 2.0

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'})
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/index.html', context=context_dict)
+    return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')
 
