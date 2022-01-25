45e964ad6e6d0f963eed5723d3f1853ff06e37c0
“Libby <2565119c@student.gla.ac.uk>
2022-01-25 14:49:39 +0000

    Chapter 5: Commit 5 (after tests - WORKING)

--- populate_rango.py
+++ populate_rango.py
@@ -59,7 +59,7 @@ def add_cat(name,views=0,likes=0):
     return c
 
 if __name__=='__main__':
-    print('Starting Rango population script...')
+    print("Starting Rango population script...")
     populate()
     
     
