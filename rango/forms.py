42a2bd78cc7c5170ded2124f8b62815c997a06e8
“Libby <2565119c@student.gla.ac.uk>
2022-02-08 18:10:54 +0000

    Chapter 9 Commit 2

--- rango/forms.py
+++ rango/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+from rango.models import UserProfile
 from rango.models import Page,Category
 
 class CategoryForm(forms.ModelForm):
@@ -29,7 +31,17 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
-    
+        
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+    class UserProfileForm(forms.ModelForm):
+        class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
+
     
     
     
